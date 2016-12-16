b894790f761c81a4c7fb521f233dae5478455dad
Malin Nemergut <mmheineking@gmail.com>
2016-12-16 14:11:00 -0500

    ingredient inputs now display in list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-$('#add').click(function(){
-  ('.new-ingredients').append($('<li>', {
-    text: $('#input-ing').val()
-  }));
-});
+var main = function(){
+  $('form').submit(function(event){
+    var $input = $(event.target).find('#input-ing');
+    var comment = $input.val();
+
+    if (comment !== ""){
+      var html = $('<li>').text(comment);
+      html.prependTo('#new-ingredients');
+      $input.val("");
+    }
+    return false;
+  });
+};
+
+
+
+$(document).ready(main);
