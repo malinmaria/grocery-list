7c16e816186463f9517a1206a216ec1f4caf79ed
Malin Nemergut <mmheineking@gmail.com>
2016-12-18 22:20:15 -0500

    check box and text input ingredients append to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 var main = function(){
   $('form').submit(function(event){
     var $input = $(event.target).find('#input-ing');
-    var comment = $input.val();
+    var ing = $input.val();
 
-    if (comment !== ""){
-      var html = $('<li>').text(comment);
-      html.prependTo('#new-ingredients');
+    if (ing !== ""){
+      var html = $('<li data-value="'+ ing +'"></li>').text(ing);
+      html.appendTo('#new-ingredients');
       $input.val("");
+    }
+    return false;
+  });
+  $('.chkbx').change(function(){
+    var value = $(this).val(),
+    $list = $('#new-ingredients');
+    if(this.checked){
+      $list.append("<li data-value='" + value + "'>" + value + "</li>");
+    }
+    else {
+        $list.find('li[data-value="' + value + '"]').slideUp("fast", function() {
+            $(this).remove();
+        });
     }
     return false;
   });
